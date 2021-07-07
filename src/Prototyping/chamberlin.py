1b883f05124d7664f839d88f40c9f348154bf22f
Juergen Hock <juergen.hock@jurihock.de>
2021-07-07 20:24:48 +0200

    Add zero-pole plot
    Not really sure if it's correct or not. Confused about numpy.polynomial.polynomial.polyroots vs scipy.signal.tf2zpk results...

--- src/Prototyping/chamberlin.py
+++ src/Prototyping/chamberlin.py
@@ -1,8 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
+from matplotlib.patches \
+    import Circle
+
+from numpy.polynomial.polynomial \
+    import polyroots as polyroots
+
 from numpy.polynomial.polynomial \
     import polyval as polyval
+
+def zplot(z, p, axis):
+
+    circle = Circle((0,0), radius=1, fill=False, linestyle='-', color='black', alpha=0.3)
+    axis.add_patch(circle)
+
+    axis.axvline(0, linestyle='--', color='black', alpha=0.3)
+    axis.axhline(0, linestyle='--', color='black', alpha=0.3)
+
+    axis.plot(z.real, z.imag, 'o', markersize=9, color='none', markeredgecolor='blue', alpha=0.7)
+    axis.plot(p.real, p.imag, 'x', markersize=9, color='red', alpha=0.7)
+
+    axis.set_xlabel('re')
+    axis.set_ylabel('im')
+
+    r = 0.5 + np.amax(np.concatenate((abs(z), abs(p), [1])))
+    axis.axis('scaled')
+    axis.axis([-r, r, -r, r])
+
+def roots(b, a):
+
+    b = np.array(b) / a[0]
+    a = np.array(a) / a[0]
+    b /= b[0]
+    z = polyroots(b)
+    p = polyroots(a)
+
+    return z, p
 
 def freqz(b, a, fs, fn):
 
@@ -35,6 +69,10 @@ lp['w'], lp['h'] = freqz(b=lp['b'], a=lp['a'], fs=FS, fn=FN)
 hp['w'], hp['h'] = freqz(b=hp['b'], a=hp['a'], fs=FS, fn=FN)
 bp['w'], bp['h'] = freqz(b=bp['b'], a=bp['a'], fs=FS, fn=FN)
 
+lp['z'], lp['p'] = roots(lp['b'], lp['a'])
+hp['z'], hp['p'] = roots(hp['b'], hp['a'])
+bp['z'], bp['p'] = roots(bp['b'], bp['a'])
+
 figure, axes = plot.subplots(nrows=1, ncols=2)
 
 figure.suptitle('Chamberlin \nFS={}Hz, FC={}Hz, \nQ={}'.format(FS, FC, Q))
@@ -58,5 +96,17 @@ axes[1].set_xlabel('Frequency [Hz]')
 axes[1].set_ylabel('Phase [rad]')
 axes[1].legend()
 axes[1].grid()
+
+figure, axes = plot.subplots(nrows=1, ncols=3)
+
+figure.suptitle('Chamberlin \nFS={}Hz, FC={}Hz, \nQ={}'.format(FS, FC, Q))
+
+zplot(lp['z'], lp['p'], axes[0])
+zplot(hp['z'], hp['p'], axes[1])
+zplot(bp['z'], bp['p'], axes[2])
+
+axes[0].set_title('LP')
+axes[1].set_title('HP')
+axes[2].set_title('BP')
 
 plot.show()
